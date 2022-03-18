2fd02d4ee520ea00dcfc1f845000f75691bf1b0a
Waleed Arshad Awan <waleedmalik677@gmail.com>
2022-03-19 04:27:01 +0500

    dont let mint

--- src/components/Countdown/Countdown.element.js
+++ src/components/Countdown/Countdown.element.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-   height: 100%;
-  width: 100%;
+  min-height: 120vh;
+  max-width: 120vw;
   position: fixed;
   top: 0;
   left: 0;
@@ -20,8 +20,7 @@ export const Wrapper = styled.div`
   color: #fff;
   color: #fff4cf;
   font-family: "wonder";
-
-  
+  z-index: 11;
   
 `;
 
